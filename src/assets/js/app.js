6abd0de0569d47ad56ea8c1c055dac2afc949ec8
Finn Engels <finn-troops@web.de>
2018-03-07 20:04:35 +0100

    initial commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+window.jQuery = $;
+
+import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+
+$(document).foundation();
+
